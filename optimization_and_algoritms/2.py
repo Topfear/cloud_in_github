75332171ed7123dab3f82fe2212e6041ff60d8f6
mitrofanov <dimanax3@yandex.ru>
2019-02-19 11:57:20 +0300

    2 exersize complete with python3

--- optimization_and_algoritms/2.py
+++ optimization_and_algoritms/2.py
@@ -1,9 +1,5 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 # В единственной строке входного файла INPUT.TXT записано единственное целое число N, не превышающее по абсолютной величине 104.
 # В единственную строку выходного файла OUTPUT.TXT нужно вывести одно целое число — сумму чисел, расположенных между 1 и N включительно.
-
 
 
 def main():
@@ -12,7 +8,7 @@ def main():
 		input_value = int(f.readline())
 
 	with open('OUTPUT.TXT', 'w') as f:
-		f.write(str(sum([x for x in xrange(1, input_value + 1)])))
+		f.write(str(sum([x for x in range(1, input_value + 1)])))
 
 
 if __name__ == '__main__':
