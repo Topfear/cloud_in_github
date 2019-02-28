005e19610dec14f238cb722338810bf3e8f6d45f
mitrofanov <dimanax3@yandex.ru>
2019-02-28 13:29:30 +0300

    i dont know

--- optimization_and_algoritms/112.py
+++ optimization_and_algoritms/112.py
@@ -4,17 +4,16 @@ def main():
     push_ups = 0
 
     with open('INPUT.TXT', 'r') as f:
-        line_len = int(f.readline().split()[0])
-        while True:
+        line_len, rows = list(map(lambda x: int(x), f.readline().split()[:2]))
+        for row in range(rows):
             line = f.readline()
-            if not line:
-                break
             line = [int(x) for x in line.split()]
 
+            memory = [0 for x in range(line_len)]
             for index in range(line_len):
                 human_height = line[index]
-                for x in range(index, line_len):
-                    push_ups += 1 if human_height > line[x] else 0
+                push_ups += sum(memory[human_height:])
+                memory[human_height - 1] = 1
                     
 
     with open('OUTPUT.TXT', 'w') as f:
