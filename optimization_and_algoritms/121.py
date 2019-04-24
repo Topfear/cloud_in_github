d0a67305a659ae8fb9c84445489b1d29939baf5a
mitrofanov <dimanax3@yandex.ru>
2019-04-24 15:26:25 +0300

    121

--- optimization_and_algoritms/121.py
+++ optimization_and_algoritms/121.py
@@ -19,42 +19,24 @@ def main():
 
     with open('INPUT.TXT', 'r') as f:
         nails_count = int(f.readline())
-        nails_position = list(map(lambda x: [int(x), False], f.readline().split()))
+        nails_position = list(map(lambda x: int(x), f.readline().split()))
 
     fibers = 0
 
     nails_position.sort()
 
-    # Соединим первого и следущего
-    fibers += nails_position[1][0] - nails_position[0][0]
-    nails_position[1][1] = True
-    nails_position[0][1] = True
+    d = list(0 for x in range(nails_count))
+    d[0] = 0
+    d[1] = nails_position[1] - nails_position[0]
+    for i in range(2, nails_count - 2):
+        d[i] = min(nails_position[i-1], nails_position[i+1])
 
-    # Соединим последнего и предыдущего, если необходимо
-    if not nails_position[-1][1]:
-        fibers += nails_position[-1][0] - nails_position[-2][0]
-        nails_position[-1][1] = True
-        nails_position[-2][1] = True
-
-
-    for index, nail in enumerate(nails_position):
-        if nail[1]:
-            continue
-        
-        _prev, _next = nails_position[index-1], nails_position[index+1]
-        to_prev, to_next = nail[0] - _prev[0], _next[0] - nail[0]
-
-        if to_prev < to_next:
-            fibers += to_prev
-        else:
-            fibers += to_next
-            _next[1] = True
-
-        nail[1] = True
+    print(d)
+    print(nails_position)
 
 
     with open('OUTPUT.TXT', 'w') as f:
-    	f.write(str(fibers))
+    	f.write(str(d[-1]))
 
 if __name__ == '__main__':
     main()
